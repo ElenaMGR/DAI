c6b0eb1e71e77c1bd2f123552f35bc8924f1b724
ElenaMGR <elenamgr@correo.ugr.es>
2020-10-22 15:18:09 +0200

    Add css and html

--- flask/app/app.py
+++ flask/app/app.py
@@ -31,17 +31,20 @@ def ordena(cadena):
 	fin = time.time()
 	listaBurbuja = [str(i) for i in listaBurbuja]
 	resBurbuja = "Burbuja ha tardado: " + str(fin - inicio) + " segundos"
-	listaOrdBurbuja = "Lista ordenada mediante Burbuja: " + (' - ').join(listaBurbuja)
 
 	inicio = time.time()
 	listaSeleccion=seleccion(lista[:])
 	fin = time.time()
 	listaSeleccion = [str(i) for i in listaSeleccion]
 	resSeleccion = "Selección ha tardado: " + str(fin - inicio) + " segundos"
-	listaOrdSeleccion = "Lista ordenada mediante Selección: " + (' - ').join(listaSeleccion)
 
-	cadenaFinal = "Ordenando..." + (' - ').join(listaBurbuja)
-	return cadenaFinal
+	#return cadenaFinal
+	lista = [str(i) for i in lista]
+	return render_template('ordena.html',
+			original =(' - ').join(lista), 
+			ordenada =(' - ').join(listaBurbuja),
+			burbuja = resBurbuja,
+			seleccion = resSeleccion)
 
 # Muestra una página para el ejercicio 3 de la Práctica 1
 @app.route('/criba/<num>')
@@ -49,39 +52,55 @@ def cribaEratostenes(num):
 	if num.isdigit():
 		numerosPrimos = criba(int(num))
 		numerosPrimos = [str(i) for i in numerosPrimos]
-		return "Números primos hasta " + num + ": " + (', ').join(numerosPrimos)
+		msg = "Números primos hasta " + num + ": " + (', ').join(numerosPrimos)
 	else:
-		return "Debe introducir un número."
+		msg = "Debe introducir un número."
+
+	return render_template('ejercicios.html',
+			ejercicio = "Criba de Eratóstenes",
+			mensaje = msg)
 
 # Muestra una página para el ejercicio 4 de la Práctica 1
 @app.route('/fibonacci/<num>')
 def fibonacci(num):
 	if num.isdigit():
 		sucesion = sfibonacci(int(num))
-		return "El numero de la sucesión de Fibonacci en la posición " + num + " es: " + str(sucesion)
+		msg = "El numero de la sucesión de Fibonacci en la posición " + num + " es: " + str(sucesion)
 	else:
-		return "Debe introducir un número."
+		msg = "Debe introducir un número."
+	
+	return render_template('ejercicios.html',
+			ejercicio = "Sucesión de Fibonacci",
+			mensaje = msg)
 
 # Muestra una página para el ejercicio 5 de la Práctica 1
 @app.route('/balanceo/<cadena>')
 def balanceo(cadena):
-	return balanceada(cadena)
+	return render_template('ejercicios.html',
+			ejercicio = "Balanceo",
+			mensaje = balanceada(cadena))
 
 # Muestra una página para el ejercicio 6 de la Práctica 1
 @app.route('/expresiones_regulares/palabraEspMayus/<cadena>')
 def erPalabraEspMayus(cadena):
-	return palabraEspMayus(cadena)
+	return render_template('ejercicios.html',
+			ejercicio = "Expresiones regulares",
+			mensaje = palabraEspMayus(cadena))
 
 @app.route('/expresiones_regulares/correo/<cadena>')
 def erCorreo(cadena):
-	return correo(cadena)
+	return render_template('ejercicios.html',
+			ejercicio = "Expresiones regulares",
+			mensaje = correo(cadena))
 
 @app.route('/expresiones_regulares/tarjeta/<cadena>')
 def erTarjeta(cadena):
-	return tarjeta(cadena)
+	return render_template('ejercicios.html',
+			ejercicio = "Expresiones regulares",
+			mensaje = tarjeta(cadena))
 
 # Muestra una página de error
 @app.errorhandler(404)
 def error_404(error):
-	return "Error 404, page not found", 404
+	return render_template('error.html'), 404
 
