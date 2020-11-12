9d56a07df50f69ecc2297a683cd8cfbee0d2cd83
ElenaMGR <elenamgr@correo.ugr.es>
2020-11-12 18:13:41 +0100

    P3 empezada

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,9 +1,12 @@
 #./app/app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 from ejercicios import *
 import time
+
+p3 = True
 
 @app.route('/')
 @app.route('/index')
@@ -140,3 +143,20 @@ def random_svg():
 	fig=fig+' stroke='+color+' stroke-width=4 fill='+color_relleno
 
 	return render_template('svg.html', figura=fig)
+
+#########################################################
+#														#
+#					Práctica 3							#
+#														#
+#########################################################
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or \
+				request.form['password'] != 'secret':
+			error = 'Invalid Username or password'
+		else:
+			return redirect(url_for('index'))
+	return render_template('login.html', error=error)
